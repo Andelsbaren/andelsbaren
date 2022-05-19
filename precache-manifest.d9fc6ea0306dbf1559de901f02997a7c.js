92ac4d295deee2134178fcb23a73a1b85d947c16
Eric Flodin <91188701+flemt@users.noreply.github.com>
2022-05-19 10:57:43 +0200

    Updates

---
+++ precache-manifest.d9fc6ea0306dbf1559de901f02997a7c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "77bce3244bbb13e8ab3d",
+    "url": "/static/css/main.ad5038cc.chunk.css"
+  },
+  {
+    "revision": "77bce3244bbb13e8ab3d",
+    "url": "/static/js/main.77bce324.chunk.js"
+  },
+  {
+    "revision": "6da4cb9d737b624fb782",
+    "url": "/static/js/1.6da4cb9d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d57076bee64efbf05e778e7ff0d876e8",
+    "url": "/static/media/bild1.d57076be.jpg"
+  },
+  {
+    "revision": "7eb494a45a9e0f951890c527cf644f82",
+    "url": "/static/media/BAS.7eb494a4.jpg"
+  },
+  {
+    "revision": "379f02f1fb316a0579472f859b2a3d33",
+    "url": "/static/media/bild3.379f02f1.jpg"
+  },
+  {
+    "revision": "b5a96bdfbfd47bdbf51451bdec6e97a5",
+    "url": "/static/media/aft3.b5a96bdf.png"
+  },
+  {
+    "revision": "2848dc714e4927e73d24a5019cc69d72",
+    "url": "/static/media/bild6.2848dc71.jpg"
+  },
+  {
+    "revision": "40b51807ee54a4822d99818798dbecd0",
+    "url": "/static/media/bild7.40b51807.jpg"
+  },
+  {
+    "revision": "41a32fa3532a0a6e49f6685c17c36b1c",
+    "url": "/static/media/bild8.41a32fa3.jpg"
+  },
+  {
+    "revision": "543227880169309055f6cd6008d53a64",
+    "url": "/static/media/bild5.54322788.jpg"
+  },
+  {
+    "revision": "09fea8604920a6c8e4006ca028579d7e",
+    "url": "/static/media/svenskaspel_square.09fea860.avif"
+  },
+  {
+    "revision": "bfa1121bdfe39cea776898b7eae20777",
+    "url": "/static/media/atg-money-icon-rect-60x60-01 (2).bfa1121b.svg"
+  },
+  {
+    "revision": "2a2b831600c3fdc30420dae53ac6bf26",
+    "url": "/index.html"
+  }
+];
